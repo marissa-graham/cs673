b639c4f3e633f1062c5467ad0d2b0783beb39a12
marissa-graham <marissa.graham101@gmail.com>
2018-02-24 01:05:39 -0700

    I MADE BABBLE I MADE BABBLE I MADE BABBLE

--- corpus.py
+++ corpus.py
@@ -1,125 +1,187 @@
 #!python3
 import numpy as np
 import phonetic
+import string
 from scipy import sparse
+from matplotlib import pyplot as plt
 
 class WordCorpus:
-	"""
-	Calculate the transition probabilities for a given corpus of text and 
-	use the distribution to generate options for new text.
+    """
+    Calculate the transition probabilities for a given corpus of text and 
+    use the distribution to generate options for new text.
 
-	Attributes:
-		- size (int): Number of unique words in the corpus.
-		- wordSequence: NumPy array where the i-th element is the index of 
-		  the i-th word in the corpus text.
-		- wordList: List of unique Word objects in the corpus.
-		- wordDict: Dictionary with wordDict[Word.word] = index of Word.
-		  That is, we look up the index based on the text of an actual Word,
-		  so we can extend the corpus without duplicating Word objects.
-		- A (csr_matrix): Transition matrix.
-	"""
+    Attributes:
+        - size (int): Number of unique words in the corpus.
+        - length (int): Total number of words in the corpus.
+        - wordSeq: NumPy array where the i-th element is the index of 
+          the i-th word in the corpus text.
+        - wordList: List of unique Word objects in the corpus.
+        - wordDict: Dictionary with wordDict[Word.word] = index of Word.
+          That is, we look up the index based on the text of an actual Word,
+          so we can extend the corpus without duplicating Word objects.
+        - A (csr_matrix): Transition matrix.
+    """
 
-	def __init__(self):
-		"""
-		Initialize an empty corpus.
-		"""
-		self.corp_string = None
-		self.size = None
-		self.wordSequence = None
-		self.wordList = []
-		self.wordDict = dict()
-		self.A = None
+    def __init__(self, dictionary):
+        """
+        Initialize an empty corpus. Dictionary is a PhoneticDictionary object.
+        """
+        self.dictionary = dictionary
+        self.corp_string = None
+        self.size = 0
+        self.wordSeq = None
+        self.wordList = []
+        self.wordDict = dict()
+        self.A = None
 
-	def _initializeCorpus(self):
-		"""
-		Initialize wordSequence, wordList, and wordDict for a corpus given a
-		list of word strings. Updates class attributes in place.
+    def _initializeCorpus(self):
+        """
+        Initialize wordSeq, wordList, and wordDict for a corpus given a
+        list of word strings.
+        """
 
-		Arguments:
-			words: A list of strings with the corpus words.
-		"""
-		pass
+        # Split the corpus string into words
+        wordstrings = self.corp_string.split()
 
-	def _initializeMatrix(self):
-		"""
-		Initialize the transition matrix as a scipy.sparse.csr_matrix.
+        # Initialize variables before iterating through the words
+        self.wordSeq = np.zeros(len(wordstrings))
+        nonos = string.punctuation+'0123456789'
+        
+        for i in range(len(wordstrings)):
 
-		Arguments:
+            # Get punctuation free word text
+            word = wordstrings[i].strip(nonos).lower()
 
-		"""
-		pass
+            # If the word is already in the corpus, only update wordSeq
+            try:
+                self.wordSeq[i] = self.wordDict[word]
 
-	def _extendCorpus(self, text):
-		pass
+            # Otherwise, create the word and update accordingly 
+            except KeyError:
 
-	def _extendMatrix(self):
-		pass
+                # Look the word up in the dictionary
+                new_word = self.dictionary.lookup(word)
 
-	def initialize(self, text, is_filename=True, keeplines=False):
-		"""
-		Read in the corpus and calculate the transition matrix self.A
+                # Append to wordList 
+                self.wordList.append(new_word)
 
-		Arguments:
-			- text: String containing either a filename or the actual text.
-			- is_filename: Bool telling us whether text is a filename.
-			- keeplines: Bool telling us whether to keep line structure at 
-				the cost of sometimes splitting words apart, or to keep words
-				together at the cost of losing line structure.
-		
-		"""
+                # Update the index and use to update wordDict and wordSeq
+                self.wordDict[word] = self.size
+                self.wordSeq[i] = self.size
+                self.size += 1
 
-		# Assign the text to self.corp_string
-		if is_filename:
-			with open(text) as filename:
-				self.corp_string = filename.read()
-		else:
-			self.corp_string = text
+        print("\nInput text:", len(wordstrings), "words,", self.size, "unique")
 
-		# Remove newline characters
-		if keeplines == False:
+    def _initializeMatrix(self):
+        """
+        Initialize the transition matrix as a scipy.sparse.csr_matrix.
 
-			# Hyphens followed by a newline should be ignored
-			self.corp_string.replace('-\n','')
-			self.corp_string.replace('\n',' ')
+        We use the convention that A[i,j] is the probability of word i
+        being followed by word j.
+        """
+        n = len(self.wordSeq)
+        A_raw = sparse.coo_matrix(
+            (np.ones(n-1),(self.wordSeq[:-1], self.wordSeq[1:])),
+            shape=(self.size, self.size)
+            ).tocsr()
 
-		# Remove hyphens so hyphenated words can be treated as two words
-		self.corp_string.replace('-',' ')
+        # TODO: NORMALIZE WITHOUT DENSIFYING THE DAMN THING
+        # Normalize rows to get probabilities
+        #self.A = A_raw.copy()
+        #rowsums = A_raw.sum(axis=1)
+        #self.A.data /= rowsums.repeat(np.diff(self.A.indptr))
+        self.A = A_raw / np.maximum(A_raw.sum(axis=1),np.ones(self.size))
 
-		self._initializeCorpus()
-		self._initializeMatrix()
+    def _extendCorpus(self, text):
+        pass
 
-	def extend(self, text, is_filename=True):
-		pass
+    def _extendMatrix(self):
+        pass
 
-	def add_constraints(self):
-		pass
+    def initialize(self, text, is_filename=True, keeplines=False):
+        """
+        Read in the corpus and calculate the transition matrix self.A
+
+        Arguments:
+            - text: String containing either a filename or the actual text.
+            - is_filename: Bool telling us whether text is a filename.
+            - keeplines: Bool telling us whether to keep line structure at 
+                the cost of sometimes splitting words apart, or to keep words
+                together at the cost of losing line structure.
+        """
+
+        # Assign the text to self.corp_string
+        if is_filename:
+            with open(text) as filename:
+                self.corp_string = filename.read()
+        else:
+            self.corp_string = text
+
+        #print("Raw corp_string:", self.corp_string)
+
+        # Remove newline characters
+        if keeplines == False:
+
+            # Hyphens followed by a newline should be ignored
+            self.corp_string = self.corp_string.replace('-\n','')
+            self.corp_string = self.corp_string.replace('\n',' ')
+
+        # Remove hyphens so hyphenated words can be treated as two words
+        self.corp_string = self.corp_string.replace('-',' ')
+
+        #print("\nTidied corp_string:", self.corp_string)
+
+        self._initializeCorpus()
+        self._initializeMatrix()
+
+    def extend(self, text, is_filename=True):
+        pass
+
+    def add_constraints(self):
+        pass
+
+    def generate_babble(self, n):
+        """
+        Generate n words of babble.
+        """
+        #current = np.random.randint(self.size)
+        current = 0
+        babble_words = [self.wordList[current].word.lower()]
+        self.A = np.asarray(self.A)
+
+        for i in range(1,n):
+            row = self.A[current,:]
+            current = np.random.choice(self.size, p=self.A[current,:])
+            babble_words.append(self.wordList[current].word.lower())
+
+        print("\n", " ".join(babble_words))
 
 class Syllable:
-	"""
-	Does this need to go here or in phonetic? Yes
-	
-	Four pieces (all ARPABET symbols)
-	- Beginning consonant list
-	- Vowel sound
-	- Ending consonant list
-	- Stress
+    """
+    Does this need to go here or in phonetic? Yes
+    
+    Four pieces (all ARPABET symbols)
+    - Beginning consonant list
+    - Vowel sound
+    - Ending consonant list
+    - Stress
 
-	rhyme ending
-		a syllable has to have a vowel sound in the middle, and can optionally
-		begin or end in 
-	"""
-	pass
+    rhyme ending
+        a syllable has to have a vowel sound in the middle, and can optionally
+        begin or end in 
+    """
+    pass
 
 
 class StructureCorpus:
-	"""
-	Calculate the transition probabilities for rhyme and meter in order to
-	use the distribution to generate new structures.
+    """
+    Calculate the transition probabilities for rhyme and meter in order to
+    use the distribution to generate new structures.
 
-	1) Brute force: Go through all of the words/chunks of syllables and 
-	   ask "does it rhyme with anything we've seen yet"
-	   - By the word? By the syllable? By chunks of syllables?
-	2) Learning of what patterns to pay attention to
-	3) Simplify to a small number of patterns
-	4) Generate new patterns accordingly
-	"""
+    1) Brute force: Go through all of the words/chunks of syllables and 
+       ask "does it rhyme with anything we've seen yet"
+       - By the word? By the syllable? By chunks of syllables?
+    2) Learning of what patterns to pay attention to
+    3) Simplify to a small number of patterns
+    4) Generate new patterns accordingly
+    """

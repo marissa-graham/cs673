41bedcb3dcb037b35e316937b6392d71e6788c5d
reedperkins <reedperkins32@gmail.com>
2018-03-09 17:28:44 -0700

    Test.py commit

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from rhymetools import RhymeEvaluator as rm
+from phonetic import PhoneticDictionary
 
 if __name__ == "__main__":
-    print(rm.evaluate("enable", "fable"))
-
+    # print(rm.evaluate("enable", "fable"))
+    from phonetic import PhoneticDictionary
+    pd = PhoneticDictionary("data/cmudict/cmudict.0.7a")
+    word1 = pd.lookup("table")
+    word2 = pd.lookup("relabel")
+    print(word1)
+    print(word2)
     pass
 
 

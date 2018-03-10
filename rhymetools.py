4443a7c292dda86a9d17fe682f6cf7fe752c4eb2
reedperkins <reedperkins32@gmail.com>
2018-03-09 17:37:29 -0700

    Added evaluate() skeleton for RhymeEvaluator

--- rhymetools.py
+++ rhymetools.py
@@ -8,49 +8,20 @@ class RhymeEvaluator:
         pass
 
     @classmethod
-    def evaluate(cls, a, b):
+    def evaluate(cls, word1, word2, i, j, priority):
         """
-        Evaluates how well two words rhyme
+        Returns the rhyme score by evaluating the onset, nucleus, and code of two words
         Args:
-            a: word 1
-            b: word 2
+            word1: the first word
+            word2: the second word
+            i: idx of nucleus of first word
+            j: idx of nucleus of second word
+            priotity: a tuple (x,x,x) containing weights for onset, nucleus, and coda
 
-        Returns:
-        A number between 0-1 indicating how well the two words
+        Returns: A value between 0-1 indicating how closly aligned the stress tails of both words are
         """
-        """
-        Naive implementation: 
-        
-        1. Take two words. For example:
-            word 1: table
-            word 2: stable
-        
-        2. Right align them so that their end syllables match:
-                table |
-               stable |
-        
-        3. Starting from the last syllable, traverse backwards until reaching 
-            the first syllable of the shorter word. Count number of matches
-                table |
-               stable |
-                ^^^^^
-        4. Return the numbers of matched syllables/total number of syllables
-        """
-        dictionary = cmudict.dict()
-        try:
-            word1 = dictionary[a][0]
-            word2 = dictionary[b][0]
-        except KeyError as k:
-            print("{} not found in dictionary".format(k))
-        else:
-            count = 0
-            l_word1 = len(word1)
-            l_word2 = len(word2)
-            length =  l_word1 if l_word1 < l_word2 else l_word2
-            for i in range(1, length + 1):
-                if word1[-i] == word2[-i]:
-                    count += 1
-            return count * 2 / (l_word1 + l_word2)
+        return 0.5
+
     """
     def _rhyme(w, level):
         entries = cmudict.entries()

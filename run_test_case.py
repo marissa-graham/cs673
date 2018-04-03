bb4a80623f00472e9b69e00cd92acf5f50160898
marissa-graham <marissa.graham101@gmail.com>
2018-04-02 23:15:59 -0600

    Initialize a blank file to make running test cases easier

---
+++ run_test_case.py
@@ -0,0 +1,18 @@
+#!python3
+import itertools
+import nltk
+import re
+import string
+import numpy as np
+from scipy import sparse
+
+import phonetic
+import corpus
+import rhymetools
+import verse
+import generate_sample
+
+"""
+Just an outline for all the stuff you need to have, know, call, and do to run
+a full test case.
+"""
